6e273ccf2f0c38cbbe7f3e36127a3ad1cce98299
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-03-13 21:14:15 +0530

    feat: Lifting the state - Part1

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import { useState } from "react";
 import Square from "./components/Square";
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
